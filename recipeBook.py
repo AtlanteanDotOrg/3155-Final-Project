593f95ea900656f9a1caf553d4aa1f2ddbd8e182
Gavran <gdodson3@uncc.edu>
2023-04-06 19:00:23 -0400

    feat: rough start on POST method

---
+++ recipeBook.py
@@ -0,0 +1,62 @@
+from flask import Flask, flash, redirect, url_for, render_template, session
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
+
+DB_HOST = "localhost"
+DB_NAME = "grandmas_recipe_book"
+DB_USERNAME = "root"
+DB_Password = "GamingGuy42"
+
+database_file = f"mysql+pymysql://{DB_USERNAME}:{DB_Password}@{DB_HOST}:3306/{DB_NAME}"
+
+app = Flask(__name__)
+app.secret_key = "mysecret"
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+
+
+class Recipe():
+    __tablename__ = 'grandmas_recipe_book.recipes'
+    id = db.Column(db.Integer, primary_key=True)
+    Recipe_Name = db.Column(db.String(45), nullable=False)
+    Recipe_Description = db.Column(db.String(500), nullable=False)
+    IsVegan = db.Column(db.Boolean, nullable=True)
+    IsGlutenFree = db.Column(db.Boolean, nullable=True)
+
+    def __init__(self, Recipe_Name, Recipe_Description, IsVegan, IsGlutenFree):
+        self.recipe_name = Recipe_Name
+        self.recipe_desc = Recipe_Description
+        self.isVegan = IsVegan
+        self.isGlutenFree = IsGlutenFree
+
+
+@app.route("/")
+def home():
+    return render_template("site/index.html")
+
+
+@app.route("/recipes")
+def about():
+    return render_template("site/about.html")
+
+
+@app.route('/addrecipe', methods=['GET', 'POST'])
+def add_recipe():
+    if request.method == 'POST':
+        if not request.form['Recipe_Name'] or not request.form['Recipe_Description'] or not request.form['isVegan'] or not request.form['isGlutenFree']:
+            flash('Please enter all the fields', 'error')
+        else:
+            recipe = Recipe(request.form['Recipe_Name'], request.form['Recipe_Description'], request.form['isVegan'], request.form['isGlutenFree'])
+
+            db.session.add(recipe)
+            db.session.commit()
+
+            flash('Record was successfully added')
+            return redirect(url_for('recipe'))
+    return render_template('site/post.html')
+
+
+if __name__ == '__main__':
+    app.run(port=3001, host="localhost", debug=True)
